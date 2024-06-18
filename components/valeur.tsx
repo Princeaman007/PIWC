b7c9d178a675d16a13b46ff1df968b6d9f7b98c6
prince <princeaman635@gmail.com>
2024-06-18 23:08:29 +0200

    improve page valeurs

--- components/valeur.tsx
+++ components/valeur.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 const Nosvaleur = () => {
     return (
-        <div className='h-56 text-center'>
-
-            Nos Valeur
+        <div className="h-56 text-center mt-9 ">
+            <span className="text-5xl font-bold ">Nos Valeurs</span>
+            
         </div>
     );
 };
