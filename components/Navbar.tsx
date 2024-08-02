4ed3b262a71000488010ebe19165974be3b9a0f4
prince <princeaman635@gmail.com>
2024-08-02 17:09:06 +0200

    responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,17 +7,21 @@ import {
 } from "@/components/ui/menubar";
 //import Image from 'next/image';
 import Image from "next/image";
+import { DropdownMenuDemo } from "./dropdownmenu";
 
 
 export default function Navbar() {
     return (
 
-        <header className="bg-white shadow-md sticky top-0 z-50">
-            <div className="container mx-auto flex justify-between items-center py-4 px-6">
+        <header className="bg-white shadow-md sticky top-0 z-50 grid grid-cols-1 md:grid-cols-12">
+            <div className="container mx-auto py-4 px-6 md:col-start-2 md:col-span-10 flex items-center justify-between">
                 <div className="text-xl font-bold">
                     <Link href="/">PIWC BRUSSELS</Link>
                 </div>
-                <Menubar className="border-0">
+                <div className="flex md:hidden">
+                   <DropdownMenuDemo/>
+                </div>
+                <Menubar className="border-0 hidden md:flex">
                     <MenubarMenu>
                         <MenubarTrigger className="text-gray-800 hover:text-orange-700 transition duration-300 ease-in-out">
                             <Link href="/">Accueil</Link>
@@ -34,6 +38,7 @@ export default function Navbar() {
                     </MenubarMenu>
                 </Menubar>
             </div>
+            
         </header>
 
     );
