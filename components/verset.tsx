b25befbd1fc3984c200cd5485000587c5166ff59
prince <princeaman635@gmail.com>
2024-08-13 17:22:36 +0200

    improv

--- components/verset.tsx
+++ components/verset.tsx
@@ -16,7 +16,7 @@ const Verset = () => {
       <div className="absolute inset-0 bg-black bg-opacity-80 flex items-center justify-center z-10 p-4">
         <div className="text-white space-y-4 text-center ">
           <p className='text-lg md:text-2xl'>
-          "For God so loved the world that he gave his only begotten Son, that whoever believes in him should not perish but have eternal life."
+          For God so loved the world that he gave his only begotten Son, that whoever believes in him should not perish but have eternal life.
           </p>
           <span className="block text-center text-5xl font-bold ">Jean 3:16</span>
           
