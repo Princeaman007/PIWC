b576e1cf4dec703c466dbae8ed0a30b57a8b1014
prince <princeaman635@gmail.com>
2024-08-13 17:14:10 +0200

    improv photo

--- components/nouveau.tsx
+++ components/nouveau.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const CirclePhotoCollage = () => {
   return (
-    <div className="container mx-auto p-4 flex flex-col items-center h-screen">
+    <div className="container mx-auto p-4 flex flex-col items-center h-screen mb-14">
             <h2 className="text-3xl font-extrabold text-gray-900 mb-4 text-center">WHO ARE WE</h2>
             <p className="text-center max-w-xl mb-28 text-gray-700">
             This is an example of a circular photo collage. You can use this layout to showcase multiple images in a visually appealing circular arrangement. Customize the images and content as per your needs.
